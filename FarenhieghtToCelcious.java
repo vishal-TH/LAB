4256f9848a1aa00f8a747463bfe27f425f4b81de
Vishal Kumar <vishal-th@hotmail.com>
2022-07-23 14:11:52 +0530

    Create FarenhieghtToCelcious.java

---
+++ FarenhieghtToCelcious.java
@@ -0,0 +1,11 @@
+public class FarenhieghtToCelcious {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celcious = 98, forenheight;
+		forenheight = (9 / 5) * celcious + 32;
+		System.out.println(forenheight);
+
+	}
+
+}
