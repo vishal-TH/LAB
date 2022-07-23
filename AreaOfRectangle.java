fca1bb84c85849167917442932670268eef4ee4c
Vishal Kumar <vishal-th@hotmail.com>
2022-07-23 14:10:00 +0530

    Create AreaOfRectangle.java

---
+++ AreaOfRectangle.java
@@ -0,0 +1,13 @@
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double areaOfSqure, length = 10, hieght = 20, areaOfRectangle, side = 4;
+
+		areaOfSqure = side * side;
+		areaOfRectangle = length * hieght;
+		System.out.println(areaOfSqure);
+		System.out.println(areaOfRectangle);
+	}
+
+}
