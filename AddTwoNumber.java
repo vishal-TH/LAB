b775e727a62a455d934b95b749a25642316e50eb
Vishal Kumar <vishal-th@hotmail.com>
2022-07-23 14:09:24 +0530

    Create AddTwoNumber.java

---
+++ AddTwoNumber.java
@@ -0,0 +1,12 @@
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		System.out.println(sum);
+
+	}
+
+}
